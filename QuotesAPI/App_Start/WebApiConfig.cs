b75a17793a0c0424e304399207e1b159cb02ccff
Katie Sallows <ktsallows@gmail.com>
2022-03-06 16:38:52 -0500

    response formatting

--- QuotesAPI/App_Start/WebApiConfig.cs
+++ QuotesAPI/App_Start/WebApiConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Net.Http.Headers;
 
 namespace QuotesAPI
 {
@@ -19,6 +20,10 @@ namespace QuotesAPI
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
+            //config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+            config.Formatters.JsonFormatter.SerializerSettings.Formatting = Newtonsoft.Json.Formatting.Indented;
         }
     }
 }
