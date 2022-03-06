650e357490719bec3b362dd0050b42c04c06fc15
Katie Sallows <ktsallows@gmail.com>
2022-03-06 15:59:51 -0500

    implement put and delete

--- QuotesAPI/Controllers/QuotesController.cs
+++ QuotesAPI/Controllers/QuotesController.cs
@@ -26,18 +26,31 @@ namespace QuotesAPI.Controllers
         }
 
         // POST: api/Quotes
-        public void Post([FromBody]string value)
+        public void Post([FromBody]Quote quote)
         {
+            quotesDBContext.Quotes.Add(quote);
+            quotesDBContext.SaveChanges(); //need this for changes to save to db...
         }
 
         // PUT: api/Quotes/5
-        public void Put(int id, [FromBody]string value)
+        public void Put(int id, [FromBody]Quote quote)
         {
+            // lambda expressions
+            // (input-parameters) => expression
+            // takes parameter q, and returns matches where the q.Id is the id given for update
+            var entity = quotesDBContext.Quotes.FirstOrDefault(q => q.Id == id);
+            entity.Title = quote.Title;
+            entity.Description = quote.Description;
+            entity.Author = quote.Author;
+            quotesDBContext.SaveChanges();
         }
 
         // DELETE: api/Quotes/5
         public void Delete(int id)
         {
+            var quote = quotesDBContext.Quotes.Find(id);
+            quotesDBContext.Quotes.Remove(quote);
+            quotesDBContext.SaveChanges();
         }
     }
 }
