45559332549629126d16321233caf9f693387b9b
Katie Sallows <ktsallows@gmail.com>
2022-03-06 16:17:02 -0500

    status codes added

--- QuotesAPI/Controllers/QuotesController.cs
+++ QuotesAPI/Controllers/QuotesController.cs
@@ -13,44 +13,61 @@ namespace QuotesAPI.Controllers
     {
         QuotesDBContext quotesDBContext = new QuotesDBContext();
         // GET: api/Quotes
-        public IEnumerable<Quote> Get()
+        public IHttpActionResult Get()
         {
-            return quotesDBContext.Quotes;
+            var quotes = quotesDBContext.Quotes;
+            //return StatusCode(quotes);
+            return Ok(quotes);
         }
 
         // GET: api/Quotes/5
-        public Quote Get(int id)
+        public IHttpActionResult Get(int id)
         {
             var quote = quotesDBContext.Quotes.Find(id);
-            return quote;
+            if (quote == null)
+            {
+                return NotFound();
+            }
+            return Ok(quote);
         }
 
         // POST: api/Quotes
-        public void Post([FromBody]Quote quote)
+        public IHttpActionResult Post([FromBody]Quote quote)
         {
             quotesDBContext.Quotes.Add(quote);
             quotesDBContext.SaveChanges(); //need this for changes to save to db...
+            return StatusCode(HttpStatusCode.Created);
         }
 
         // PUT: api/Quotes/5
-        public void Put(int id, [FromBody]Quote quote)
+        public IHttpActionResult Put(int id, [FromBody]Quote quote)
         {
             // lambda expressions
             // (input-parameters) => expression
             // takes parameter q, and returns matches where the q.Id is the id given for update
-            var entity = quotesDBContext.Quotes.FirstOrDefault(q => q.Id == id);
-            entity.Title = quote.Title;
-            entity.Description = quote.Description;
-            entity.Author = quote.Author;
+            var existingQuote = quotesDBContext.Quotes.FirstOrDefault(q => q.Id == id);
+            if (quote == null)
+            {
+                return BadRequest("Not found");
+            }
+            existingQuote.Title = quote.Title;
+            existingQuote.Description = quote.Description;
+            existingQuote.Author = quote.Author;
             quotesDBContext.SaveChanges();
+            return Ok("Quote updated");
         }
 
         // DELETE: api/Quotes/5
-        public void Delete(int id)
+        public IHttpActionResult Delete(int id)
         {
             var quote = quotesDBContext.Quotes.Find(id);
+            if (quote == null)
+            {
+                return BadRequest("Not found");
+            }
             quotesDBContext.Quotes.Remove(quote);
             quotesDBContext.SaveChanges();
+            return Ok("Quote deleted");
         }
     }
 }
