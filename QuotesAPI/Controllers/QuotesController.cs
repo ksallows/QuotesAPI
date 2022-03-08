eca8a5753b1cfbe19c143d8bbcdeff1ab66d864b
Katie Sallows <ktsallows@gmail.com>
2022-03-07 19:37:57 -0500

    changed get route name; cleanup some statements that are now 1 line

--- QuotesAPI/Controllers/QuotesController.cs
+++ QuotesAPI/Controllers/QuotesController.cs
@@ -12,32 +12,26 @@ namespace QuotesAPI.Controllers
     public class QuotesController : ApiController
     {
         QuotesDBContext quotesDBContext = new QuotesDBContext();
-        // GET: api/Quotes
-        public IHttpActionResult Get()
+        // GET: api/GetQuotes
+        public IHttpActionResult GetQuotes()
         {
             var quotes = quotesDBContext.Quotes;
             //return StatusCode(quotes);
             return Ok(quotes);
         }
 
-        // GET: api/Quotes/5
-        public IHttpActionResult Get(int id)
+        // GET: api/GetQuote/5
+        public IHttpActionResult GetQuote(int id)
         {
             var quote = quotesDBContext.Quotes.Find(id);
-            if (quote == null)
-            {
-                return NotFound();
-            }
+            if (quote == null) return NotFound();
             return Ok(quote);
         }
 
         // POST: api/Quotes
         public IHttpActionResult Post([FromBody]Quote quote)
         {
-            if(!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
+            if (!ModelState.IsValid) return BadRequest(ModelState);
             quotesDBContext.Quotes.Add(quote);
             quotesDBContext.SaveChanges(); //need this for changes to save to db...
             return StatusCode(HttpStatusCode.Created);
@@ -46,18 +40,12 @@ namespace QuotesAPI.Controllers
         // PUT: api/Quotes/5
         public IHttpActionResult Put(int id, [FromBody]Quote quote)
         {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
+            if (!ModelState.IsValid) return BadRequest(ModelState);
             // lambda expressions
             // (input-parameters) => expression
             // takes parameter q, and returns matches where the q.Id is the id given for update
             var existingQuote = quotesDBContext.Quotes.FirstOrDefault(q => q.Id == id);
-            if (quote == null)
-            {
-                return BadRequest("Not found");
-            }
+            if (quote == null) return BadRequest("Not found");
             existingQuote.Title = quote.Title;
             existingQuote.Description = quote.Description;
             existingQuote.Author = quote.Author;
@@ -69,10 +57,7 @@ namespace QuotesAPI.Controllers
         public IHttpActionResult Delete(int id)
         {
             var quote = quotesDBContext.Quotes.Find(id);
-            if (quote == null)
-            {
-                return BadRequest("Not found");
-            }
+            if (quote == null) return BadRequest("Not found");
             quotesDBContext.Quotes.Remove(quote);
             quotesDBContext.SaveChanges();
             return Ok("Quote deleted");
